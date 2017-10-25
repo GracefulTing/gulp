8092326c27a1f9a51c47687009c8de35d8c361f9
tt <953632795@qq.com>
2017-10-25 19:12:37 +0800

    add file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# gulp
+# lesson-1
+
+### sudo cnpm install gulp -g   全局安装gulp
+### cd 文件夹
+### npm init --yes   package.json
+### cnpm install gulp --save        安装gulp
+### gulp 运行
+### sudo cnpm uninstall gulp -g 卸载
+### gulp message 具体任务
+### cnpm install gulp-copy --save-dev
+### cnpm install gulp-imagemin --save-dev
+### cnpm install gulp-uglify --save-dev
+### gulp watch 运行完改动
+### cnpm install gulp-webserver --save-dev
---
+++ gulpapp/gulpfile.js
@@ -0,0 +1,89 @@
+const gulp = require("gulp");     //ES6中const    可使用var
+const gulpCopy = require("gulp-copy");
+const minImage = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const webserver = require("gulp-webserver");
+
+/*
+	* ------常用方法------
+	* gulp.task   --- 定义任务
+	* gulp.src    --- 输入文件路径
+	* gulp.dest   --- 输出文件路径
+	* gulp.watch  --- 监听文件变化
+	
+	* ------node方法------
+	* pipe   --- 管道
+*/
+
+
+// //定义默认任务
+// gulp.task('default', () => {
+// 	console.log("default是必须要这么写,才是默认任务!");
+// });
+
+//定义具体任务
+gulp.task("message", () => {
+	console.log("执行具体任务,需要在终端运行gulp message,即gulp 任务名");
+});
+
+//定义拷贝任务
+gulp.task("copyHtml", () => {       //任务名随便起
+	//找到需要拷贝文件的路径
+	gulp.src('src/*.html')
+		.pipe(gulp.dest('dist'));
+});
+
+//定义压缩图片任务
+gulp.task("imageMin", () => {
+	gulp.src('src/images/*')
+		.pipe(minImage())
+		.pipe(gulp.dest('dist/images'));
+});
+
+//定义压缩JS代码任务
+	//1.下载模块--gulp-uglify
+	//2.引入模块--require
+	//3.定义任务--task
+	//4.在src下创建一个js文件夹,创建一个js,随便写一个函数
+	//5.实现任务
+gulp.task("jsMin", () => {
+	gulp.src('src/js/*')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));;
+});
+
+//定义sass转css任务
+gulp.task("sass", () => {
+	gulp.src('src/sass/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'));
+})
+
+//定义默认任务
+gulp.task('default', ["message","copyHtml","imageMin","jsMin","sass"]);
+
+//监听任务
+gulp.task('watch',function(){
+	gulp.watch("src/js/*.js",["jsMin"]);
+	gulp.watch("src/images/*",["imageMin"]);
+	gulp.watch("src/sass/*.scss",["sass"]);
+	gulp.watch("src/*.html",["copyHtml"]);
+});
+
+//webserver
+gulp.task('webserver',function(){
+	return gulp.src('app')
+			   .pipe(webserver({
+			   		port:8080,
+			   		livereload:true,       //热更新
+			   		open:true
+			   }));
+});
+
+gulp.task('watch1',function(){
+	gulp.watch("app",["webserver"]);
+});
+
+//运行webserver后进行更改可以自动更新.
+
